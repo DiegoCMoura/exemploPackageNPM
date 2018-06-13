7c82104a7bd77183ba32bbba09f171522fe57fa4
Diego Camara Moura <Diego.Moura@adtalembrasil.com.br>
2018-06-13 16:36:15 -0300

    Commit initial.

---
+++ src/angular-x-minimal-npm-package.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule  } from '@angular/common';
+
+import { AngularXMinimalNpmPackageComponent } from './angular-x-minimal-npm-package.component';
+
+@NgModule({
+  imports: [ CommonModule ],
+  declarations: [ AngularXMinimalNpmPackageComponent ],
+  exports:  [ AngularXMinimalNpmPackageComponent ],
+  entryComponents: [ AngularXMinimalNpmPackageComponent ],
+})
+export class AngularXMinimalNpmPackageModule {}
